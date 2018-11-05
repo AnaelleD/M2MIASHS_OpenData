2035d6bd5f4038694f954a07fa9a6cd4027cc52a
AnaelleD <anaelle.dambreville@gmail.com>
2018-11-05 14:26:22 +0100

    Script fetch

---
+++ themeSport.js
@@ -0,0 +1,11 @@
+getSport = function(){
+  var url = 'https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?s=Soccer&c=France'
+
+  fetch(url).then(
+    function(u){ return u.json();}
+  ).then(
+    function(json){
+      console.log(json);
+    }
+  )
+}
