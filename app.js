d90564431f98592ede27cf375215047d53f4a53f
samadeleine <sarah.madeleine@hotmail.fr>
2018-11-06 16:39:00 +0100

    ajout du tirage aléatoire sans remise

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.get('/flag', function(req, res){
   request('https://restcountries.eu/rest/v2/region/europe', function(error, reponse, body){
+    //console.log(body)
+
     //définir des array pour récupérer les noms et les drapeaux
     var flag = [];
     var name = [];
@@ -18,12 +20,31 @@ app.get('/flag', function(req, res){
     //console.log(flag)
     //res.send(name) //affiche la liste des noms de pays
 
-    var nbQuestion = [];
+    //tirage au sort sans remise du noms des pays pour une session de quizz avec 10 questions
+    var nameQuestion = new Array();
+    var nameDispo = new Array();
+    for (i in name){
+      nameDispo.push(name[i]);
+    };
+
+    console.log(nameDispo)
+
+    for (var i = 0; i<10; i++)
+    	{
+      var longueur = nameDispo.length;
+    	tirage = Math.floor(Math.random() * longueur);
+    	nameQuestion[i] = nameDispo[tirage];
+    	nameDispo.splice(tirage,1);
+    };
+    console.log(nameDispo.length)
+
+    //tirage au sort avec remise
+    /*var nameQuestion = [];
     for (i=0; i<10; i++){
-    nbQuestion[i] = Math.floor(Math.random()*name.length);
-  };
-    console.log(nbQuestion);
-    res.send(nbQuestion);
+      nameQuestion[i] = Math.floor(Math.random()*name.length);
+    };*/
+    //console.log(nameQuestion);
+    res.send(nameQuestion);
   });
 });
 
