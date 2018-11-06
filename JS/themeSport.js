9c4fed73b18138e40a4c40efb7c72b8555b250eb
AnaelleD <anaelle.dambreville@gmail.com>
2018-11-06 15:58:29 +0100

    maj req sports

--- JS/themeSport.js
+++ JS/themeSport.js
@@ -2,9 +2,6 @@ getSport = function(){
   request('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?s=Soccer&c=France' , function(error, response, body){
   console.log(body)
   jsonListeSport = JSON.parse(body)
-  //str = jsonListeSport.Actors
-  //var splt = str.split(",")
-  //console.log(splt[0])
-  console.log(jsonListeSport)
+  return jsonListeSport
   })
 }
--- application/routes/mainroutesSport.js
+++ application/routes/mainroutesSport.js
@@ -1,5 +1,4 @@
 const request = require('request')
-//const music = require('../../JS/thememusic.js')
 //const cheerio = require('cheerio')
 //const fs = require('fs')
 //const Json2csvparser = require('json2csv').Parser
@@ -11,16 +10,17 @@ module.exports = function(app, express) {
     const mainRoutes = express.Router()
 
     mainRoutes.get('/', function(req, res) {
-			res.render('index')
-		})
-
-    mainRoutes.get('/themeSport.js', function(req, res) {
-      fs.readFile("JS/themeSport.js'", function(err, data) {
-       res.writeHead(200, {'Content-Type': 'application/json'})
-       res.write(data)
-       res.end()
+      request('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?s=Soccer&c=France' , function(error, response, body){
+      jsonListeSport = JSON.parse(body)
+      teams = jsonListeSport.teams
+      var listeTeam = []
+      for (iteam in teams){
+        listeTeam.push(teams[iteam].strTeam)
+      }
+      console.log(listeTeam)
+      })
     })
-  })
+
 
 	// apply the routes to our application
     app.use('/', mainRoutes)
