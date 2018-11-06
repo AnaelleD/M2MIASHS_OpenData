2a8992acbe6867812659e01453be343a25d9f865
Rym <31435778+RymMichaut@users.noreply.github.com>
2018-11-06 11:46:47 +0100

    création du json titres_films.json

--- themeFilm.js
+++ themeFilm.js
@@ -1,11 +1,18 @@
-var Film
+const request = require('request')
 
-getFilm = function(){
-  request('http://www.omdbapi.com/?t=Film&apikey=25f31d56' , function(error, response, body){
+const Film = {}
+
+Film.getFilm = function(res, titre){
+  request('http://www.omdbapi.com/?t='+ titre +'&apikey=25f31d56' , function(error, response, body){
     console.log(body)
     jsonomdb = JSON.parse(body)
     str = jsonomdb.Actors
     var splt = str.split(",")
+
     console.log(splt[0])
+
+    res.send(splt[0])
   })
 }  
+
+module.exports = Film
