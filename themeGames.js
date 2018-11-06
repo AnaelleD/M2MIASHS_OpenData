cd4e93f7bc6f2cb43846cd0ba5d35a8e08f9084a
Serrah <sara387.thomas@hotmail.fr>
2018-11-06 10:48:09 +0100

    Update themeGames.js

--- themeGames.js
+++ themeGames.js
@@ -1,5 +1,5 @@
 getGames = function(){
-	var req = "https://query.wikidata.org/sparql?query=SELECT%20%3FgameLabel%20WHERE%20%7B%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%22.%20%7D%0A%20%20%3Fgame%20wdt%3AP31%20wd%3AQ7889.%0A%7D&format=json"
+	var req = "https://query.wikidata.org/sparql?query=SELECT%20%3Fgame%20%3FgameLabel%20%3Flogo%20WHERE%20%7B%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%22.%20%7D%0A%20%20%3Fgame%20wdt%3AP31%20wd%3AQ7889.%0A%20%20%3Fgame%20wdt%3AP154%20%3Flogo.%0A%7D&format=json"
 	fetch(req).then(
     function(u){ return u.json();}
   ).then(
