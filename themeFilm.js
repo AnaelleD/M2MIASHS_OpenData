c2a951c2de64ac9b3e73bf586b2ef5dda312c840
Rym <31435778+RymMichaut@users.noreply.github.com>
2018-11-06 10:11:54 +0100

    modification du fichier themeFilm.js

--- themeFilm.js
+++ themeFilm.js
@@ -1,14 +1,11 @@
 var Film
 
 getFilm = function(){
-  var url = 'http://www.omdbapi.com/?t=Film'
-
-  fetch(url).then(
-    function(u){ return u.json();}
-  ).then(
-    function(json){
-      
-      console.log(json);
-    }
-  )
-}
+  request('http://www.omdbapi.com/?t=Film&apikey=25f31d56' , function(error, response, body){
+    console.log(body)
+    jsonomdb = JSON.parse(body)
+    str = jsonomdb.Actors
+    var splt = str.split(",")
+    console.log(splt[0])
+  })
+}  
