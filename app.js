573adb58bf0faf39bb8d07444d9e07b8ec001a98
samadeleine <sarah.madeleine@hotmail.fr>
2018-11-07 11:12:27 +0100

    modification colonne faux1 et disposition dictionnaire nameQuestion

--- app.js
+++ app.js
@@ -12,32 +12,29 @@ app.get('/flag', function(req, res){
     //parcourir chaque élément du JSON et extraire le nom et le drapeau du pays
     for (i in jsonFlag) {
       name.push(jsonFlag[i].name); //liste contenant les noms des pays
-      flag.push({ key: jsonFlag[i].name , value: jsonFlag[i].flag}); //dictionnaire contenant les noms et drapeaux des pays
+      flag.push({ question: jsonFlag[i].flag , reponse: jsonFlag[i].name}); //dictionnaire contenant les noms et drapeaux des pays
     };
 
     //tirage au sort sans remise du noms des pays pour une session de quizz avec 10 questions
-    var nameQuestion = new Array();
-    var exclure = new Array();
-    var falseNameDispo = new Array();
-    //création de la liste des pays et drapeaux disponibles pour faire le tirage
-    var nameDispo = new Array();
-    for (i in flag){
-      nameDispo.push(flag[i]);
-    };
+    var nameQuestion = [];
+    var exclure = [];
+    var falseNameDispo = [];
+
     //tirage au sort sans remise
     for (var i = 0; i<10; i++){
-      var longueur = nameDispo.length;
+      var longueur = flag.length;
     	tirage = Math.floor(Math.random() * longueur);
-    	nameQuestion[i] = nameDispo[tirage];
-    	exclure.push(nameDispo.splice(tirage,1));
-      falseNameDispo[i] = nameDispo.splice(exclure,1);
-      nameQuestion[i].faux1 = falseNameDispo[i][0].key + i;
+    	nameQuestion[i] = flag[tirage];
+    	exclure.push(flag.splice(tirage,1));
+      falseNameDispo[i] = name.splice(exclure,1);
+      nameQuestion[i].faux1 = falseNameDispo[i][0];
     };
     //ajout d'une colonne de réponses fausses
 
 
 
     console.log(falseNameDispo);
+    console.log(nameQuestion);
     res.send(nameQuestion);
   });
 });
