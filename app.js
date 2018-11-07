b8cf759b4835fa85dd113d8ea18c35d57d09138a
samadeleine <sarah.madeleine@hotmail.fr>
2018-11-07 10:30:48 +0100

    ajout 1 colonne de fausses réponses

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const app = express();
 
 app.get('/flag', function(req, res){
   request('https://restcountries.eu/rest/v2/region/europe', function(error, reponse, body){
-    //console.log(body)
-
     //définir des array pour récupérer les noms et les drapeaux
     var flag = [];
     var name = [];
-
     //définir la variable contenant le JSON récupéré par la requête
     jsonFlag = JSON.parse(body);
     //parcourir chaque élément du JSON et extraire le nom et le drapeau du pays
@@ -17,39 +14,31 @@ app.get('/flag', function(req, res){
       name.push(jsonFlag[i].name); //liste contenant les noms des pays
       flag.push({ key: jsonFlag[i].name , value: jsonFlag[i].flag}); //dictionnaire contenant les noms et drapeaux des pays
     };
-    //console.log(flag)
-    //res.send(name) //affiche la liste des noms de pays
 
     //tirage au sort sans remise du noms des pays pour une session de quizz avec 10 questions
-  //  function (){
-      var nameQuestion = new Array();
-      var nameDispo = new Array();
-      for (i in flag){
-        nameDispo.push(flag[i]);
-      };
-
-  //  }
-    //console.log(nameDispo)
-
-    for (var i = 0; i<10; i++)
-    	{
+    var nameQuestion = new Array();
+    var exclure = new Array();
+    var falseNameDispo = new Array();
+    //création de la liste des pays et drapeaux disponibles pour faire le tirage
+    var nameDispo = new Array();
+    for (i in flag){
+      nameDispo.push(flag[i]);
+    };
+    //tirage au sort sans remise
+    for (var i = 0; i<10; i++){
       var longueur = nameDispo.length;
     	tirage = Math.floor(Math.random() * longueur);
     	nameQuestion[i] = nameDispo[tirage];
-    	nameDispo.splice(tirage,1);
+    	exclure.push(nameDispo.splice(tirage,1));
+      falseNameDispo[i] = nameDispo.splice(exclure,1);
+      nameQuestion[i].faux1 = falseNameDispo[i][0].key + i;
     };
-    //console.log(nameDispo.length)
+    //ajout d'une colonne de réponses fausses
 
-    //tirage au sort avec remise
-    /*var nameQuestion = [];
-    for (i=0; i<10; i++){
-      nameQuestion[i] = Math.floor(Math.random()*name.length);
-    };*/
 
-    //console.log(nameQuestion);
+
+    console.log(falseNameDispo);
     res.send(nameQuestion);
-
-
   });
 });
 
