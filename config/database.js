1f4691fd7b3162adfaf45cf238579fb2c17914c1
Anaelle <30436079+AnaelleD@users.noreply.github.com>
2018-12-19 19:44:54 +0100

    Suppression pwd connection mlab

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 // config/database.js
 module.exports = {
-    'url' : 'mongodb://gameUser:1motdepasse@ds153763.mlab.com:53763/quizz_culture_g'
-    // process.env.urlMongoDBIAUTH
+    'url' : process.env.urlMongoDBIAUTH
     // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 }
